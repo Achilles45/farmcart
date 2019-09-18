8d80102974fcf9fa8227efc3752dd79e2ef0fcdc
Achilles <achillesusuoyibo7@gmail.com>
2019-09-18 17:12:38 +0100

    First design for navbar and hero section

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,13 +1,19 @@
 <template>
   <div class="home">
-    
+    <Navbar />
+    <Banner />
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-
+import Navbar from "@/components/Navbar.vue";
+import Banner from "@/components/Banner.vue";
 
 export default { 
+  components:{
+    Navbar,
+    Banner
+  }
 }
 </script>
